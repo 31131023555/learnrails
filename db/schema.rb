c979a0b22f465e5f8ed6c0d66b84ddd494930040
hungnt <acv12345>
2018-01-17 18:01:35 +0700

    Fix timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@ ActiveRecord::Schema.define(version: 20180117105326) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
