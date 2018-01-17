1e3408084b09db91262c84979ffc864ee64a680d
hungnt <acv12345>
2018-01-17 17:58:40 +0700

    Create categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117105122) do
+ActiveRecord::Schema.define(version: 20180117105326) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
